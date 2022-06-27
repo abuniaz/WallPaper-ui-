4196fc245afb0759aae61c2926e9dc727e1a2a46
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-27 19:23:07 +0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,29 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return DefaultTabController(
+        length: 9,
+        child: Scaffold(
+          body: SafeArea(
+              child: Column(
+            children: [
+              Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 60,
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.menu,
+                        size: 25,
+                      ),
+                      Text('WallPapers',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold))
+                    ],
+                  ))
+            ],
+          )),
+        ));
   }
 }
