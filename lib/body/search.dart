c455c13e90bd3b6fa7e7d482fa8bfb3b98c7e801
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-29 21:33:19 +0600

    Update search.dart

--- lib/body/search.dart
+++ lib/body/search.dart
@@ -5,6 +5,19 @@ class SearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      height: 55,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 10)
+          ]),
+    );
   }
 }
